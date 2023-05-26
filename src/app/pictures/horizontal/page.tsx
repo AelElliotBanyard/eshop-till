938b526364b63055e5ebeaf240f88ed8dc9aed03
aelbanyard <ael.banyard@lernende.bbw.ch>
2023-05-26 15:03:17 +0200

    add main routes

---
+++ src/app/pictures/horizontal/page.tsx
@@ -0,0 +1,9 @@
+const Horizontal = () => {
+  return (
+    <div>
+      <h1>Horizontal</h1>
+    </div>
+  );
+};
+
+export default Horizontal;
---
+++ src/app/pictures/vertical/page.tsx
@@ -0,0 +1,9 @@
+const Vertical = () => {
+    return (
+        <div>
+            <h1>Vertical</h1>
+        </div>
+    );
+};
+
+export default Vertical;
