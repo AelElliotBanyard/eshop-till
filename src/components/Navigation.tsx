28d234824c5c6ba331014e1c9357c9a736f9574a
aelbanyard <ael.banyard@lernende.bbw.ch>
2023-05-31 07:57:37 +0200

    add styling for active tab

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,42 +1,50 @@
+"use client";
 import NavigationItem from "./NavigationItem";
-import { AiOutlineShoppingCart, AiFillCaretDown } from "react-icons/ai";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { usePathname } from "next/navigation";
 
 const Navigation = () => {
-    return (
-      <div className="navigation">
-        <nav>
-          <ul className="navigationItems">
-            <li className="navigationItem">
-              <NavigationItem title="Home" path="/" list={false} />
-            </li>
-            <li className="navigationItem">
-              <NavigationItem title="Pictures" path="/pictures" list={false} />
-            </li>
-            <li className="navigationItem">
-              <NavigationItem
-                title="About"
-                path="/"
-                list={true}
-                subroutes={[
-                  { title: "About Me", path: "/" },
-                  {
-                    title: "Portfolio",
-                    path: "https://tillgrezet.myportfolio.com/",
-                  },
-                ]}
-              />
-            </li>
-            <li className="navigationItem">
-              <NavigationItem
-                title={<AiOutlineShoppingCart className="cartIcon" size={20} />}
-                path="/cart"
-                list={false}
-              />
-            </li>
-          </ul>
-        </nav>
-      </div>
-    );
+  const current = usePathname().split("/")[1];
+  console.log(current);
+  return (
+    <div className="navigation">
+      <nav>
+        <ul className="navigationItems">
+          <li className={"navigationItem " + (current === "" ? "active" : "")}>
+            <NavigationItem title="Home" path="/" list={false} />
+          </li>
+          <li
+            className={
+              "navigationItem " + (current === "pictures" ? "active" : "")
+            }
+          >
+            <NavigationItem title="Pictures" path="/pictures" list={false} />
+          </li>
+          <li className={"navigationItem " + (current === "aboutMe" ? "active" : "")}>
+            <NavigationItem
+              title="About"
+              path="/"
+              list={true}
+              subroutes={[
+                { title: "About Me", path: "/aboutMe" },
+                {
+                  title: "Portfolio",
+                  path: "https://tillgrezet.myportfolio.com/",
+                },
+              ]}
+            />
+          </li>
+          <li className={"navigationItem " + (current === "cart" ? "active" : "")}>
+            <NavigationItem
+              title={<AiOutlineShoppingCart className="cartIcon" size={20} />}
+              path="/cart"
+              list={false}
+            />
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Navigation;
