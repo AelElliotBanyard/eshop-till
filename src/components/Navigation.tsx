a226c7edb6c2f743a8d831476db3529c46484374
aelbanyard <ael.banyard@lernende.bbw.ch>
2023-05-31 07:43:20 +0200

    fix all styling to be universal

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,16 +3,39 @@ import { AiOutlineShoppingCart, AiFillCaretDown } from "react-icons/ai";
 
 const Navigation = () => {
     return (
-        <div className="navigation">
-            <nav>
-                <ul className="navigationItems">
-                    <li className="navigationItem"><NavigationItem title="Home" path="/" list={false} /></li>
-                    <li className="navigationItem"><NavigationItem title="Pictures" path="/pictures" list={false} /></li>
-                    <li><NavigationItem title="About" path="/" list={true} subroutes={[{title: "About Me", path: "/"}, {title: "Portfolio", path: "https://tillgrezet.myportfolio.com/"}]} /></li>
-                    <li className="navigationItem"><NavigationItem title={<AiOutlineShoppingCart className="cartIcon" size={20} />} path="/cart" list={false} /></li>
-                </ul>
-            </nav>
-        </div>
+      <div className="navigation">
+        <nav>
+          <ul className="navigationItems">
+            <li className="navigationItem">
+              <NavigationItem title="Home" path="/" list={false} />
+            </li>
+            <li className="navigationItem">
+              <NavigationItem title="Pictures" path="/pictures" list={false} />
+            </li>
+            <li className="navigationItem">
+              <NavigationItem
+                title="About"
+                path="/"
+                list={true}
+                subroutes={[
+                  { title: "About Me", path: "/" },
+                  {
+                    title: "Portfolio",
+                    path: "https://tillgrezet.myportfolio.com/",
+                  },
+                ]}
+              />
+            </li>
+            <li className="navigationItem">
+              <NavigationItem
+                title={<AiOutlineShoppingCart className="cartIcon" size={20} />}
+                path="/cart"
+                list={false}
+              />
+            </li>
+          </ul>
+        </nav>
+      </div>
     );
 };
 
