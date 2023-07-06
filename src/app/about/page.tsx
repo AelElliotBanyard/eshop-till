5a15f7c57cc7a49043883cd0dfc72e348c3a34fd
Evan <evan.lueber@stud.kbw.ch>
2023-07-06 10:24:05 +0200

    text in about added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+"use client";
+import React, { use } from "react";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <div className="about">
+      <div className="aboutContainer">
+        <h1 className="aboutTitle">About Me</h1>
+        <p className="aboutText">
+          Hello, I'm Till Grezet <br className="br"/> I'm a 22 year old photographer based in
+          Switzerland. <br className="br"/>Through my camera, I capture the beauty of different
+          landscapes, cultures, and architectural wonders. <br className="br"/>My goal is to inspire
+          others by freezing moments in time and telling visual stories that
+          evoke emotions. <br className="br"/>Join me on this journey as we explore the world
+          through the lens. <br className="br" />Prints with a Personal Touch: Discover my
+          photography collection and find the perfect print for your Home.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
